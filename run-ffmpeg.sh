b46f188f9d2a98e7af42dfee87aacfc7316751ad
David Helkowski <David.Helkowski1@T-Mobile.com>
2019-10-22 16:30:52 -0700

    Config and output cleanup
    Cleanup needed config so that it is only the things that need to be configured.
    Added defaults for config options to make that possible.
    
    Removed old config options that are no longer used.
    
    Pass wdaPort and vidPort through to stf when a device is connected, so that stf provider can support multiple devices in the near future.
    
    Use ascii formatter for logrus for the moment.
    
    Quiet output from ffmpeg.
    
    Cleanup run-stf script greatly to remove uneeded code and output.

--- run-ffmpeg.sh
+++ run-ffmpeg.sh
@@ -3,5 +3,4 @@ BIN="$1"
 PIPE="$2"
 DEVNAME="$3"
 shift 3
-echo Running $BIN $* into $PIPE
 $BIN -f avfoundation -i "$DEVNAME" $* > $PIPE
--- run-stf.sh
+++ run-stf.sh
@@ -1,43 +1,20 @@
 #!/bin/sh
 
 IFACE=$1
-export SUPPORT_ROOT=`jq .support_root config.json -j`
-export MYSTF_ROOT=`jq .stf_root config.json -j`
-export STF_URI=`jq .stf_ip config.json -j` # "192.168.255.1"
-export STF_URI_DNS=`jq .stf_hostname config.json -j`
-export OPENSTF_HOME="$MYSTF_ROOT"
+STF_IP=`jq .stf_ip config.json -j`
+STF_HOSTNAME=`jq .stf_hostname config.json -j`
+PUBLIC_IP=`ifconfig $IFACE | grep inet | cut -d\  -f2`
+PROVIDER_IDENT=`hostname | tr -d "\n"`
 
-STF_ROOT=`jq .stf_root config.json -j`
-if [ ! -f "${STF_ROOT}/package.json" ]; then
-  echo "STF folder ${STF_ROOT} does not exist!"
-  exit 1
-fi
-# Potentially also the folder could contain a .xctestrun file instead
-# TODO: Write correct code to check for that
-#if [ ! -d "${WDA_ROOT}/WebDriverAgent.xcodeproj" ]; then
-#  echo "WebDriverAgent folder ${WDA_ROOT} does not exist!"
-#  exit 1
-#fi
-
-echo "STF ROOT   : ${MYSTF_ROOT}"
-echo "STF_URI    : ${STF_URI}"
-echo "STF_URI_DNS: ${STF_URI_DNS}"
-
-export MIN_PORT=7400
-export MAX_PORT=7700
-export FQDN_OR_IP=`ifconfig $IFACE | grep inet | cut -d\  -f2`
-export STF_SERVER_IP="$STF_URI"
-export STF_CLIENT_IP="$FQDN_OR_IP"
-export PROVIDER_IDENT=`hostname | tr -d "\n"`
-cd $MYSTF_ROOT
-
+cd repos/stf
 node --inspect=127.0.0.1:9230 runmod.js provider \
 	--name "macmini/${PROVIDER_IDENT}" \
-	--connect-sub tcp://${STF_URI}:7250 \
-	--connect-push tcp://${STF_URI}:7270 \
-	--storage-url https://${STF_URI_DNS}/ \
-	--public-ip ${FQDN_OR_IP} \
-	--min-port=${MIN_PORT} \
-	--max-port=${MAX_PORT} \
+	--connect-sub tcp://${STF_IP}:7250 \
+	--connect-push tcp://${STF_IP}:7270 \
+	--storage-url https://${STF_HOSTNAME}/ \
+	--public-ip ${PUBLIC_IP} \
+	--min-port=7400 \
+	--max-port=7700 \
 	--heartbeat-interval 10000 \
+	--server-ip=${STF_IP} \
 	--no-cleanup
