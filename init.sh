52e3684037973f86085aaae122bce641f3704302
David Helkowski <David.Helkowski1@T-Mobile.com>
2020-02-06 14:39:30 -0800

    Improve init.sh script/process
    Previous init.sh script would not properly handle new dependencies beign installed if more are needed.
    Updated the process so that it will work properly.
    The new changes are much faster also at verifying that needed brew packages are installed.

--- init.sh
+++ init.sh
@@ -15,16 +15,10 @@ function install_brew_if_needed() {
 function install_node8_if_needed() {
   NODE_MAJOR_VERSION="none"
   if [ -f "/usr/local/opt/node@8/bin/node" ]; then
-    NODE_VERSION=`/usr/local/opt/node@8/bin/node --version | tr -d "\n"`
-    NODE_MAJOR_VERSION=`echo $NODE_VERSION | perl -pe 's/v([0-9]+).+/$1/'`
-  fi
-  
-  if [ ! $NODE_MAJOR_VERSION == "8" ]; then
-    echo -e "${RED}Node 8 not installed; installed node version: \"$NODE_MAJOR_VERSION\"$RST"
-    echo -e "Installing Node 8 and setting up symlink"
-    brew install node@8
-  else
     echo -e "${GR}Node 8 installed$RST"
+  else
+    echo -e "Installing Node 8"
+    brew install node@8
   fi
 }
 
@@ -52,34 +46,9 @@ function assert_has_xcodebuild() {
   fi
 }
 
-rm pipe
-mkfifo pipe
-
 install_brew_if_needed
-
-USBINFO=$( brew info usbmuxd | grep Cellar/libusbmuxd | cut -d\/ -f6 | cut -d\  -f1 | cut -d\. -f1,2 | cut -d\- -f1 )
-if [ "$USBINFO" == "" ]; then
-  brew install --HEAD usbmuxd
-else
-  if [ "$USBINFO" != "HEAD" ]; then
-    brew uninstall usbmuxd --ignore-dependencies
-    brew install --HEAD usbmuxd
-  fi
-fi
-
-LIBIINFO=$( brew info libimobiledevice | grep Cellar/libimobiledevice | cut -d\/ -f6 | cut -d\  -f1 | cut -d\. -f1,2 | cut -d\- -f1 )
-if [ "$LIBIINFO" == "" ]; then
-  brew install --HEAD libimobiledevice
-else
-  if [ "$LIBIINFO" != "HEAD" ]; then
-    brew uninstall libimobiledevice --ignore-dependencies
-    brew install --HEAD libimobiledevice
-  fi
-fi
-
+./util/brewser.pl ensurehead libusbmuxd
+./util/brewser.pl ensurehead libimobiledevice
 install_node8_if_needed
 assert_has_xcodebuild
-
-CACHE_FILE=$(brew --cache -s ./stf_ios_support.rb|tr -d "\n")
-cp empty.tgz "${CACHE_FILE}"
-HOMEBREW_NO_AUTO_UPDATE=1 brew install --build-from-source ./stf_ios_support.rb
+./util/brewser.pl installdeps stf_ios_support.rb
---
+++ util/brewser.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl -w
+use strict;
+use JSON::PP qw/decode_json/;
+use Data::Dumper;
+
+my $GR="\033[32m";
+my $RED="\033[91m";
+my $RST="\033[0m";
+my $action = $ARGV[0] || 'help';
+
+if( $action eq 'list' ) {
+  my $pkgs = get_pkg_versions();
+  for my $pkg ( keys %$pkgs ) {
+    my $ver = $pkgs->{$pkg};
+    print "$pkg,$ver\n";
+  }
+}
+elsif( $action eq 'installdeps' ) {
+  my $rbspec = $ARGV[1] or die "Ruby spec file must be given";
+  my $spec = read_file( $rbspec );
+  my $pkgs = get_pkg_versions();
+  my @need;
+  for my $line ( split( "\n", $spec ) ) {
+    if( $line =~ m/^\s*depends_on "(.+?)"/ ) {
+      my $dep = $1;
+      if( my $ver = $pkgs->{ $dep } ) {
+        print "$GR$dep\t\t=> version $ver$RST\n";
+      }
+      else {
+        push( @need, $dep );
+      }
+    }
+  }
+  if( @need ) {
+    my $allneed = join(' ',@need);
+    print "Installing missing packages:\n";
+    print "  ".join("\n  ",@need);
+    `brew install $allneed 1>&2`;
+  }
+}
+elsif( $action eq 'info' ) {
+  my $pkg = $ARGV[1];
+  my $info = install_info( $pkg );
+  if( !$info ) {
+    print "$pkg is not installed\n";
+    exit 1;
+  }
+  print JSON::PP->new->ascii->pretty->encode( $info );
+}
+elsif( $action eq 'ensurehead' ) {
+  ensure_head( $ARGV[1] );
+}
+else {
+  print "Brewser\n
+  Usage:
+    ./brewser.pl [action] [args]
+  Actions:
+    list - list packages and versions installed
+    info [package name] - pretty print json install receipt of named package
+    ensurehead [package name] - ensure HEAD version of a package is installed
+      If a non-HEAD version is installed, it will be removed and the current HEAD installed.
+      If a HEAD version is installed, even if old, nothing will happen.
+    installdeps [ruby spec file] - install dependencies for a specified brew package spec file\n";
+}
+
+sub get_pkg_versions {
+  my %pkgs;
+  my @dirs = sort `find /usr/local/Cellar -name .brew -maxdepth 3 -type d`;
+  for my $dir ( @dirs ) {
+    $pkgs{ $1 } = $2 if( $dir =~ m|^/usr/local/Cellar/([^/]+)/([^/]+)/\.brew$| );
+  }
+  return \%pkgs;
+}
+
+sub read_file {
+  open( my $fh, "<".shift );
+  my $data;
+  { local $/ = undef; $data = <$fh>; }
+  close( $fh );
+  return $data;
+}
+
+sub install_info {
+  my ( $pkg, $ver ) = @_;
+  if( !$ver ) {
+    my $path = `find /usr/local/Cellar/$pkg -maxdepth 1 2>/dev/null | tail -1`;
+    chomp $path;
+    return 0 if( !$path );
+    my @parts = split( "/", $path );
+    $ver = pop @parts;
+  }
+  return decode_json( read_file( "/usr/local/Cellar/$pkg/$ver/INSTALL_RECEIPT.json" ) );
+}
+
+sub ensure_head {
+  my $pkg = shift;
+  my $info = install_info( $pkg );
+  my $spec = $info ? $info->{source}{spec} : '';
+  if( !$spec || $spec ne 'head' ) {
+    print "$pkg - Installing HEAD\n";
+    `brew uninstall $pkg --ignore-dependencies` if( $spec );
+    `brew install --HEAD $pkg`;
+  }
+  else {
+    print "$GR$pkg - HEAD already installed$RST\n";
+  }
+}
