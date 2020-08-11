038f4fb9f8c3c85171d99bb3e03ab97718d4d756
David Helkowski <David.Helkowski1@T-Mobile.com>
2020-08-11 15:19:37 -0700

    Prep for auto-unlock of device on coordinator start
    Add 'unlock' CLI option for coordinator to unlock a locked device without doing anything else.
    
    Refactored STF reserve and release a bit.
    
    Added is_locked function to check lock status as well as unlock function. The addition of these functions is in preparation for automatically unlocking a locked device on startup of coordinator.

--- coordinator/stf_control.go
+++ coordinator/stf_control.go
@@ -9,51 +9,48 @@ import (
     "strings"
 )
 
-func stf_reserve( config *Config, udid string ) (bool) {
+func NewStfClient() (http.Client) {
     tr := &http.Transport{
         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
     }
-    
+    return http.Client{ Transport: tr }
+}
+
+func stf_set_auth( config *Config, req *http.Request ) {
     token := config.Stf.AdminToken
+    req.Header.Set( "Authorization", "Bearer " + token )
+}
+
+func stf_do_request( config *Config, req *http.Request ) (bool, *http.Response) {
+    client := NewStfClient()
+    stf_set_auth( config, req )
+    resp, err := client.Do( req )
+    if err != nil || !strings.HasPrefix( resp.Status, "200" ) {
+        fmt.Println("Error:", err )
+        fmt.Println("Response Status:", resp.Status)
+        body, _ := ioutil.ReadAll(resp.Body)
+        fmt.Println("Response Body:", string(body))
+    
+        return false,nil
+    }
+    return true, resp
+}
+
+func stf_reserve( config *Config, udid string ) (bool) {
     json := fmt.Sprintf(`{"serial":"%s"}`,udid)
     fmt.Println("Sending:",json)
     url := fmt.Sprintf("https://%s/api/v1/user/devices", config.Stf.HostName )
     req, _ := http.NewRequest("POST", url, bytes.NewReader( []byte( json ) ) )
-    req.Header.Set( "Authorization", "Bearer " + token )
     req.Header.Set( "Content-Type", "application/json" )
-    client := http.Client{ Transport: tr }
-    resp, err := client.Do( req )
-    if err != nil || !strings.HasPrefix( resp.Status, "200" ) {
-        fmt.Println("Error:", err )
-        fmt.Println("Response Status:", resp.Status)
-        body, _ := ioutil.ReadAll(resp.Body)
-        fmt.Println("Response Body:", string(body))
     
-        return false
-    }
-    
-    return true
+    success, _ := stf_do_request( config, req )
+    return success
 }
 
 func stf_release( config *Config, udid string ) (bool) {
-    tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
-    
-    token := config.Stf.AdminToken
     url := fmt.Sprintf("https://%s/api/v1/user/devices/%s", config.Stf.HostName, udid)
     req, _ := http.NewRequest("DELETE", url, nil )
-    req.Header.Set( "Authorization", "Bearer " + token )
-    client := http.Client{ Transport: tr }
-    resp, err := client.Do( req )
-    if err != nil || !strings.HasPrefix( resp.Status, "200" ) {
-        fmt.Println("Error:", err )
-        fmt.Println("Response Status:", resp.Status)
-        body, _ := ioutil.ReadAll(resp.Body)
-        fmt.Println("Response Body:", string(body))
-        
-        return false
-    }
     
-    return true
+    success, _ := stf_do_request( config, req )
+    return success
 }
